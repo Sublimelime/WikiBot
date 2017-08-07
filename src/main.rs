548b14b189726a352944cb561e48131cf46d4e77
Noah Morton <noahmorton98@gmail.com>
2017-08-07 10:50:23 -0500

    Bot now pulls token from file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
-#[macro_use] extern crate serenity;
+#[macro_use]
+extern crate serenity;
 
+use std::fs::File;
+use std::io::prelude::*;
 use serenity::client::Client;
 
+static PREFIX: &'static str = "+";
+
 fn main() {
+    let filename = "token.txt";
+    let mut file = File::open(filename).expect("Token file not found");
+
+    let mut token = String::new();
+    file.read_to_string(&mut token).expect(
+        "Something went wrong reading the token file",
+    );
+
+    let token = token.trim(); //Remove the newline from the end of the string if present
+
     // Login with a bot token from the environment
-    let mut client = Client::new("MzQxNjE3NTk1MjU2MTQzODcy.DGDyAQ.zCRbH5wUv-kHOzfSGdvBFGhaRYg");
+    let mut client = Client::new(&token[..]);
 
-    client.with_framework(|f| f
-                          .configure(|c| c.prefix("+")) // set the bot's prefix to "+"
-                          .on("ping", ping));
-
-    client.on_message(|_context, message| {
-        println!("{}", message.content);
+    client.with_framework(|f| {
+        f.configure(|c| c.prefix(PREFIX)) // set the bot's prefix to prefix declared as global
+            .on("ping", ping)
     });
+
+    println!("Bot configured, with prefix {}, now running...", PREFIX);
 
     let _ = client.start();
 }
